19f9ce4c62c6e882b0e0c09a7afbe37f3ef0a031
Marim1611 <marimnaser813@gmail.com>
2023-05-03 13:26:26 +0300

    plot & encode

---
+++ Analysis/PredictiveQuestions/utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot(df,x,y, title):
+    plt.figure(figsize=(8,8))
+    x_axis= df[x]
+    y_axis= df[y]
+    plt.bar(x_axis, y_axis)
+    plt.title(title)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
+
+def encoding(df, method ='frequency'):
+    if method == 'frequency':
+        for col in df.columns:
+                if col in [ 'Publisher', 'Developer', 'Platform', 'Genre','Rating']:
+                    df[col] = df[col].map(df[col].value_counts())/len(df)
+    return df
