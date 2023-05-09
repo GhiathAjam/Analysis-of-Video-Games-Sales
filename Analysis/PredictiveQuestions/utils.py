147430584ab826c164192069ff4895d1f97ea4c1
Marim1611 <marimnaser813@gmail.com>
2023-05-09 22:31:19 +0300

    scatter function

--- Analysis/PredictiveQuestions/utils.py
+++ Analysis/PredictiveQuestions/utils.py
@@ -2,10 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot(df,x,y, title):
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(5,5))
     x_axis= df[x]
     y_axis= df[y]
     plt.bar(x_axis, y_axis)
+    plt.title(title)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
+
+def scatter(df,x,y, title):
+    plt.figure(figsize=(8,4))
+    x_axis= df[x]
+    y_axis= df[y]
+    plt.scatter(x_axis, y_axis)
     plt.title(title)
     plt.xlabel(x)
     plt.ylabel(y)
@@ -17,3 +27,6 @@ def encoding(df, method ='frequency'):
                 if col in [ 'Publisher', 'Developer', 'Platform', 'Genre','Rating']:
                     df[col] = df[col].map(df[col].value_counts())/len(df)
     return df
+
+
+     
