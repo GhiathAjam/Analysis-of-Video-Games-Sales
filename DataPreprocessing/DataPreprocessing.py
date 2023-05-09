a75bfdd9e31d114442fb85c57cd2d2c4e4c42ebb
Marim1611 <marimnaser813@gmail.com>
2023-05-09 22:12:37 +0300

    common outliers

--- DataPreprocessing/DataPreprocessing.py
+++ DataPreprocessing/DataPreprocessing.py
@@ -171,9 +171,10 @@ def detect_outliers(df):
         num_outliers = len(outliers)
 
         print(f'Number of outliers in {col}: {num_outliers}')
-def remove_outliers(df, cols):
+
+def remove_all_outliers(df, cols):
     '''
-    Remove outliers from certain columns in the dataset 
+    Remove outliers from all specified columns in the dataset 
     '''
     for col in cols:
         # calculate interquartile range (IQR)
@@ -196,5 +197,30 @@ def remove_outliers(df, cols):
         # remove outliers from the dataset
         df = df.drop(outliers_index, axis=0)
 
+    return df
+
+def remove_common_outliers(df, cols):
+    '''
+    Remove outliers that are common in all specified columns
+    '''
+    common_index = []
+    for col in cols:
+        # calculate interquartile range (IQR)
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1 
+        # calculate the lower and upper bound
+        lower_bound = q1 - (1.5 * iqr) 
+        upper_bound = q3 + (1.5 * iqr)
+        # get the number of outliers
+        outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
+        outliers_index = outliers.index
+        common_index.extend(outliers_index)
+
+    # remove duplicates form the list
+    common_index = list(set(common_index))
+    print(f'Number of outliers removed: {len(common_index)}')
+    #remove outliers from the dataset
+    df = df.drop(common_index, axis=0)
     return df
 
