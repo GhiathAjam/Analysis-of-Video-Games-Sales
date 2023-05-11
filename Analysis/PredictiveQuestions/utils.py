a8424da4804d5e0c619e54b61e8402a7eaa3d623
Marim1611 <marimnaser813@gmail.com>
2023-05-11 19:37:40 +0300

    normal dist plot

--- Analysis/PredictiveQuestions/utils.py
+++ Analysis/PredictiveQuestions/utils.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm, skew 
+from scipy import stats
 
 def plot(df,x,y, title):
     plt.figure(figsize=(5,5))
@@ -22,11 +25,30 @@ def scatter(df,x,y, title):
     plt.show()
 
 def encoding(df, method ='frequency'):
+    '''
+    Encode the categorical features using the given method
+    '''
     if method == 'frequency':
         for col in df.columns:
                 if col in [ 'Publisher', 'Developer', 'Platform', 'Genre','Rating']:
                     df[col] = df[col].map(df[col].value_counts())/len(df)
+    # for all columns remove rows with string values
+    # for col in df.columns:
+    #     if df[col].dtype == 'object':
+    #         df = df[df[col].str.isnumeric()]
+
+
     return df
 
+def plot_distribution(df, col):
+    '''
+    Plot the distribution of the given column
+    '''
+    sns.distplot(df[col] , fit=norm);
+    (mu, sigma) = norm.fit(df[col])
+    plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],loc='best')
+    plt.ylabel('Count')
+    plt.title(col +' distribution')
+    plt.show()
 
      
