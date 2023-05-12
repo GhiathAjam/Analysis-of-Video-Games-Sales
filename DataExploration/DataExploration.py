bbddf79e79e5e590d881b704f2a73233c7b90ea5
Mariem Muhammed <76264155+mariemzayn18@users.noreply.github.com>
2023-05-12 14:15:25 +0300

    Edited features' histogram

--- DataExploration/DataExploration.py
+++ DataExploration/DataExploration.py
@@ -18,31 +18,13 @@ def plot_hist(df):
     Plot a histogram for the top 15 values in each column
     '''
 
-    for i in range(0, 16, 4):
-        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15,5))
-
-        column1 = df.iloc[:, i]
-        column2 = df.iloc[:, i+1]
-
-        top_15_1 = column1.value_counts().nlargest(15)
-        top_15_2 = column2.value_counts().nlargest(15)
-
-        data1 = column1.where(column1.isin(top_15_1.index), 'Other').astype(str)
-        data2 = column2.where(column2.isin(top_15_2.index), 'Other').astype(str)
-
-        #plot histogram for each column
-        data1.value_counts().plot(kind='bar', ax=axes[0], title=column1.name)
-        data2.value_counts().plot(kind='bar', ax=axes[1], title=column2.name)
-
-      
-        # add xticks and xlabels
-        axes[0].set_xticklabels(axes[0].get_xticklabels(), rotation=45, ha='right')
-        axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=45, ha='right')
-
-        # add title for each figure 
-        axes[0].set_title(column1.name)
-        axes[1].set_title(column2.name)
-     
+    for i in range(8):
+        fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+        for j, col in enumerate(df.columns[i*2:(i+1)*2]):
+            # plot the histogram
+            df[col].value_counts().head(15).plot(kind='bar', ax=axes[j])
+            axes[j].set_title(col)
+            axes[j].tick_params(labelrotation=90)
     plt.show()
 
 def show_nulls(df):
